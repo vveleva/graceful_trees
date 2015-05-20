70092e820c00cd12e419fbe56291123c1e73b894
vveleva <vvveleva@gmail.com>
2015-05-19 23:36:29 -0700

    Write build methods for symmetric star tree

--- symmetric_star.rb
+++ symmetric_star.rb
@@ -1,22 +1,41 @@
-require_relative 'trees'
+require 'trees.rb'
+require 'graceful_trees.rb'
 
-class SymmetricStar
-  attr_accessor :root
+
+class SymmetricStar < Tree
   attr_reader :legs, :depth
 
-  def build_tree
+  def self.build(options)
+    tree = SymmetricStar.new(options)
+    labeling = SymmetricStarLabeling.new(options[:legs], options[:depth])
+    labeling.vlabels.each do |labels|
+      tree.root.children << SymmetricStar.build_path(labels)
+    end
 
+    tree
   end
 
-  def build_path
+  def self.build_path(labels)
+    parent = start_node = Node.new(label: labels.shift)
+    # start_node.label = labels.shift
+    # parent = start_node
+    labels.each do |label|
+      temp = Node.new
+      temp.label = label
+      parent.children << temp
+      parent = temp
+    end
 
+    start_node
   end
 
   def initialize(options)
-    @root  = options[:root]
+    super(options)
     @legs  = options[:legs]
     @depth = options[:depth]
   end
-
-
 end
+
+
+star = SymmetricStar.build(legs: 2, depth: 4)
+p star
